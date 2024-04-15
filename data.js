6cbad3e3921f900117684dad43d75c0e45b42524
Tomas Vondrak <vondrak.thomas@gmail.com>
2024-04-15 10:41:04 +0200

    starting commit

---
+++ data.js
@@ -0,0 +1,28 @@
+// Map points
+
+// Galaxy servers points
+const galaxies = [
+    { name: 'Europe - galaxy.eu', coordinates: [56.7584994, 0.7277719] },
+    { name: 'USA - galaxy.org', coordinates: [43.000000, -75.000000] },
+    { name: 'Australia - galaxy.au', coordinates: [-33.865143, 151.209900] }
+];
+
+// Pulsar servers points
+const eu_pulsars = [
+    { name: 'CESNET - Czech Pulsar', coordinates: [50.2117769, 15.3615611] },
+    { name: 'Slovak Pulsar', coordinates: [48.148598, 17.107748] },
+    { name: 'France Pulsar', coordinates: [48.8566, 2.3522] },
+    { name: 'Italy Pulsar', coordinates: [41.9028, 12.4964] },
+    { name: 'Poland Pulsar', coordinates: [52.237049, 21.017532] },
+    { name: 'Belgium Pulsar', coordinates: [50.8476, 4.3572] },
+    { name: 'Spain Pulsar', coordinates: [40.416775, -3.703790] },
+    { name: 'German Pulsar', coordinates: [48.1731131, 8.9016003] },
+    { name: 'Norway Pulsar', coordinates: [59.9492842, 10.7033581] },
+    { name: 'Turkey Pulsar', coordinates: [39.2233947, 28.7209361] }
+]
+
+// Czech runner servers points
+czech_runners = [
+    { name: 'Prague - runner', coordinates: [50.0755, 14.4378], job_num: 145 },
+    { name: 'Brno - runner', coordinates: [49.195061, 16.606836] , job_num: 90 }
+]
