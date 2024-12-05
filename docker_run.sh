9f7028b0df4a8682a42762040883a337a26ac406
Tomas Vondrak <vondrak.thomas@gmail.com>
2024-12-05 10:51:02 +0100

    settings adjustment

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,17 @@
-docker run --name gjr -p 8000:8000 -v /data/gjr_data/db.sqlite3:/app/django_server_files/db.sqlite3 -v /home/debian/debug.log:/app/django_server_files/debug.log -v /home/debian/.env:/app/django_server_files/.env -v /home/debian/settings.py:/app/django_server_files/dj_leaflet/settings.py -v /home/debian/cron.log:/var/log/cron.log -d cerit.io/tomasvondrak/gjr:mounting
+copy files to docker:
+
+scp -i /home/drakis/Documents/cesnet/ssh_keys/galaxy_visualisation /home/drakis/Documents/cesnet/galaxy_visualization/django_server_files/db.sqlite3 debian@10.1.2.21:/data/db.sqlite3 # musí se nejprv dát do ~ a pak až se sudem
+
+scp -i /home/drakis/Documents/cesnet/ssh_keys/galaxy_visualisation /home/drakis/Documents/cesnet/galaxy_visualization/django_server_files/debug.log debian@10.1.2.21:~/debug.log
+
+scp -i /home/drakis/Documents/cesnet/ssh_keys/galaxy_visualisation /home/drakis/Documents/cesnet/galaxy_visualization/django_server_files/.env debian@10.1.2.21:~/.env
+
+scp -i /home/drakis/Documents/cesnet/ssh_keys/galaxy_visualisation /home/drakis/Documents/cesnet/galaxy_visualization/django_server_files/dj_leaflet/settings.py debian@10.1.2.21:~/settings.py
+
+# ještě na serveru
+touch ~/cron.log
+
+docker run: 
+
+docker run --name gjr -p 8000:8000 -v /data/db.sqlite3:/app/django_server_files/db.sqlite3 -v /home/debian/debug.log:/app/django_server_files/debug.log -v /home/debian/.env:/app/django_server_files/.env -v /home/debian/settings.py:/app/django_server_files/dj_leaflet/settings.py -v /home/debian/cron.log:/var/log/cron.log -d cerit.io/tomasvondrak/gjr:latest
 
