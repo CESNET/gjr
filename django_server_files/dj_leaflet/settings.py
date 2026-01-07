2d2ab718aa74b1ec8917e6e73b320579398be0c4
Tomas Vondrak <vondrak.thomas@gmail.com>
2026-01-07 15:17:30 +0100

    settings update

--- django_server_files/dj_leaflet/settings.py
+++ django_server_files/dj_leaflet/settings.py
@@ -3,6 +3,9 @@ from decouple import config
 from django.core.management.utils import get_random_secret_key
 import logging
 import datetime
+from dotenv import load_dotenv
+
+load_dotenv(BASE_DIR / '.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
